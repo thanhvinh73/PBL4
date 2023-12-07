a54ed2909d65e7c84be046187703373d46fe444b
NamCrystal8 <sdgm2003@gmail.com>
2023-12-07 16:01:56 +0700

    chinh sua lai hanh dong bat dau nhan dang

--- collect_dataset/testing_data.py
+++ collect_dataset/testing_data.py
@@ -1,3 +1,4 @@
+import math
 import os
 import time
 import cv2
@@ -45,17 +46,17 @@ def print_prediction_to_csv(action: str, sequence: str, res):
 
 def testing_live():
     holis = False
-    scaling_factor = 1.5
     mp_hands = mp.solutions.hands
     model = load_model()
     model.load_weights(os.path.join(
         'trained_model', 'actionwithvalid125.keras'))
-
+    last_predict = ""
     sequence = []
     threshold = 0.7
-    count_frame = 0
 
-    cap = cv2.VideoCapture("http://192.168.1.13:81/stream")
+    cap = cv2.VideoCapture(0)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 900)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 600)
     with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
         with mp_hands.Hands(
                 min_detection_confidence=0.5,
@@ -68,47 +69,23 @@ def testing_live():
                         for hand_landmarks in results.multi_hand_landmarks:
                             mp_drawing.draw_landmarks(
                                 image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
-                            indexTip = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y
-                            indexDip = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_DIP].y
-                            middleTip = hand_landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_TIP].y
-                            middleDip = hand_landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_DIP].y
-                            ringTip = hand_landmarks.landmark[mp_hands.HandLandmark.RING_FINGER_TIP].y
-                            ringDip = hand_landmarks.landmark[mp_hands.HandLandmark.RING_FINGER_DIP].y
-                            pinkyTip = hand_landmarks.landmark[mp_hands.HandLandmark.PINKY_TIP].y
-                            pinkyDip = hand_landmarks.landmark[mp_hands.HandLandmark.PINKY_DIP].y
-                            if indexTip <= indexDip and middleTip <= middleDip and ringTip <= ringDip and pinkyTip > pinkyDip:
+                            indexTip = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP]
+                            indexDip = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_DIP]
+                            middleTip = hand_landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_TIP]
+                            middleDip = hand_landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_DIP]
+                            ringTip = hand_landmarks.landmark[mp_hands.HandLandmark.RING_FINGER_TIP]
+                            ringDip = hand_landmarks.landmark[mp_hands.HandLandmark.RING_FINGER_DIP]
+                            pinkyTip = hand_landmarks.landmark[mp_hands.HandLandmark.PINKY_TIP]
+                            pinkyDip = hand_landmarks.landmark[mp_hands.HandLandmark.PINKY_DIP]
+                            indexTip_x, indexTip_y = int(indexTip.x * frame.shape[1]), int(indexTip.y * frame.shape[0])
+                            middleTip_x, middleTip_y = int(middleTip.x * frame.shape[1]), int(middleTip.y * frame.shape[0])
+                            ringTip_x, ringTip_y = int(ringTip.x * frame.shape[1]), int(ringTip.y * frame.shape[0])
+                            distance_indexmid = math.sqrt((middleTip_x - indexTip_x)**2 + (middleTip_y - indexTip_y)**2)
+                            distance_indexring = math.sqrt((ringTip_x - middleTip_x)**2 + (ringTip_y - middleTip_y)**2)
+                            if (indexTip.y <= indexDip.y and middleTip.y <= middleDip.y and ringTip.y <= ringDip.y and pinkyTip.y > pinkyDip.y and distance_indexmid > 30):
                                 holis = True
                                 time.sleep(1)
-                            screen_width, screen_height = pyautogui.size()
-                            bbox_xmin = 0.5 - 480 / screen_width
-                            bbox_ymin = 0.5 - 270 / screen_height
-                            bbox_xmax = 0.5 + 480 / screen_width
-                            bbox_ymax = 0.5 + 270 / screen_height
-                            if indexTip <= indexDip and middleTip > middleDip and ringTip > ringDip and pinkyTip > pinkyDip:
-                                x = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x
-                                y = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y
-                                rel_x = x - bbox_xmin
-                                rel_y = y - bbox_ymin
-                                scaled_x = rel_x * scaling_factor
-                                scaled_y = rel_y * scaling_factor
-                                final_x = bbox_xmin + scaled_x
-                                final_y = bbox_ymin + scaled_y
-                                if final_x < 0:
-                                    final_x = 0
-                                elif final_x > 1:
-                                    final_x = 1
-
-                                if final_y < 0:
-                                    final_y = 0
-                                elif final_y > 1:
-                                    final_y = 1
-                                if bbox_xmin <= x <= bbox_xmax and bbox_ymin <= y <= bbox_ymax:
-                                    pyautogui.moveTo(
-                                        final_x * screen_width, final_y * screen_height)
-                                else:
-                                    pass
-                            cv2.rectangle(image, (int(bbox_xmin * image.shape[1]), int(bbox_ymin * image.shape[0])), (int(
-                                bbox_xmax * image.shape[1]), int(bbox_ymax * image.shape[0])), (0, 255, 0), 2)
+                    cv2.putText(image, last_predict, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
                     cv2.imshow('OpenCV Feed', image)
                 else:
                     image, results = mediapipe_detection(frame, holistic)
@@ -121,6 +98,7 @@ def testing_live():
                             predicted_action = actions[np.argmax(res)]
                             print(predicted_action,
                                   res[np.argmax(res)], np.argmax(res))
+                            last_predict = predicted_action
                             match np.argmax(res):
                                 case 0:
                                     pyautogui.press('right')
@@ -130,13 +108,9 @@ def testing_live():
                                     pyautogui.hotkey('alt', 'f5')
                                 case 3:
                                     pyautogui.press('esc')
-
                             holis = False
-
-                        # Reset the sequence for the next 30 frames
                         sequence = []
                     cv2.imshow('OpenCV Feed', image)
-                # Tắt camera bằng nút q
                 if cv2.waitKey(10) & 0xFF == ord('q'):
                     break
             cap.release()
