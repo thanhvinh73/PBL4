cd5cf502d5eeca8785507c83ec2c0025d1e6c55f
NamCrystal8 <sdgm2003@gmail.com>
2023-12-10 17:56:30 +0700

    fix Arduino pan tilt

--- collect_dataset/testmouse.py
+++ collect_dataset/testmouse.py
@@ -1,22 +1,14 @@
+import os
 import mediapipe as mp
 import cv2
 import numpy as np
 from mediapipe.framework.formats import landmark_pb2
-import time
-from math import sqrt
-import win32api
-import pyautogui
+from keras.utils import plot_model
+from public_data import actions, mp_holistic, mp_drawing, mediapipe_detection, draw_styled_landmarks, extract_keypoints, load_model, normalize_video, load_model_rnn
 
-import websocket 
- 
-websock_url = "ws://192.168.39.158/Camera"
-ws = websocket.WebSocket()
-ws.connect(websock_url)
-while ws.connected:
-    img = np.asanyarray(bytearray(ws.recv())) 
-    im = cv2.imdecode(img, cv2.IMREAD_COLOR)
-    
-    cv2.imshow("image", im)
-    cv2.waitKey(10)
+model = load_model()
 
+model.load_weights(os.path.join(
+        'trained_model', 'actionwithvalid125.keras'))
 
+plot_model(model, to_file='model_plot.png', show_shapes=True, show_layer_names=True, show_layer_activations=True)
